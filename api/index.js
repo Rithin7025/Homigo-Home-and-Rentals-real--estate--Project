f194a6632deb38f4081add55486882bd8159c7df
Rithin <rithinrj2001@gmail.com>
2023-10-24 16:03:43 +0530

    database connected

--- api/index.js
+++ api/index.js
@@ -1,6 +1,13 @@
 import express from 'express';
 const app = express();
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import { error } from 'console';
+dotenv.config();
 
+mongoose.connect(process.env.DATABASE_URL)
+.then(()=> console.log('Database connected'))
+.catch((error) => console.log(error))
 
 
 app.listen(3000,()=>{

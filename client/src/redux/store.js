4ca5a2a305038be83dda71fcb3e306e5d4297f3d
Rithin <rithinrj2001@gmail.com>
2023-11-09 15:49:21 +0530

    added redux-persist library to save userinfo in localStorage

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,9 +1,22 @@
 import { configureStore } from '@reduxjs/toolkit'
-import userReducer from './user/userSlice.js'
+import userReducer from './user/userSlice.js';
+
+import storage from 'redux-persist/lib/storage';
+import { persistReducer, persistStore } from 'redux-persist';
+
+
+const persistConfig = {
+    key: 'root',
+    storage,
+  }
+  
+  const persistedReducer = persistReducer(persistConfig, userReducer)
+
+
 
 export const store = configureStore({
     reducer : {
-        user : userReducer
+        user : persistedReducer
     },
     middleware : (getDefaultMiddleware) => getDefaultMiddleware({
         serializableCheck : false
@@ -11,3 +24,4 @@ export const store = configureStore({
     
 }) 
 
+export const persistor = persistStore(store)
